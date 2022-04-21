b5951f6f7aa66d6b5f14e7e700b730f1fe7a1690
Martin <sephymartin@gmail.com>
2022-04-21 12:46:26 +0800

    feat: change pacakge list

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,6 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/vernesong/OpenClash.git custom_package/OpenClash
-cp -r custom_package/OpenClash/luci-app-openclash package/
+# git clone https://github.com/vernesong/OpenClash.git custom_package/OpenClash
+# cp -r custom_package/OpenClash/luci-app-openclash package/
+sed -i '$a src-git smpackage https://github.com/kenzok78/small-package' feeds.conf.default
